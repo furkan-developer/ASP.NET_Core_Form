121c6186fd82a8015dd15a2056f6156e70228869
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-06-15 13:04:01 +0300

    Get data from HTML form element with server side validation

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -11,16 +11,38 @@ namespace ASP.NET_Form.Controllers
 {
     public class PersonsController : Controller
     {
-        private readonly List<Person> persons;
+        private readonly ICollection<Person> persons;
 
         public PersonsController(ICollection<Person> persons)
         {
-            this.persons = persons.ToList();
+            this.persons = persons;
         }
 
         public IActionResult Index()
         {
             return View(persons);
+        }
+
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply(
+        [Bind(nameof(Person.FirstName),nameof(Person.LastName))]
+        [FromForm]
+        Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                persons.Add(person);
+                return RedirectToAction(actionName:nameof(Index));
+            }
+
+            return View(person);
         }
     }
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ASP.NET_Form.Models
 {
@@ -10,7 +12,7 @@ namespace ASP.NET_Form.Models
     {
         [Required]
         public string FirstName { get; set; } = string.Empty;
-
+        
         [Required]
         public string LastName { get; set; } = string.Empty; 
     }
